c474071fc25666f59e514d8640c4b50d2a6a04d4
KamleshBMahajan <kamleshbmahajan555@gmail.com>
2023-12-09 12:20:10 +0530

    first commit

---
+++ StudentRecordManagementSystem/StudentRecordManagementSystem/Models/StudentDataAccessLayer.cs
@@ -0,0 +1,58 @@
+﻿using StudentRecordManagementSystem.Utility;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace StudentRecordManagementSystem.Models
+{
+    public class StudentDataAccessLayer
+    {
+        string connectionString = ConnectionString.CName;
+        //GetAllStudent
+        public IEnumerable<Student> GetAllStudent()
+        {
+            List<Student> lstStudent = new List<Student>();
+            using(SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spGetAllStudent", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                conn.Open();    
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Student student = new Student();
+                    student.Id = Convert.ToInt32(reader["ID"]);
+                    student.FirstName = reader["FirstName"].ToString();
+                    student.LastName = reader["LastName"].ToString();
+                    student.Email = reader["Email"].ToString();
+                    student.Mobile = reader["Mobile"].ToString();
+                    student.Address = reader["Address"].ToString();
+
+                    lstStudent.Add(student);
+
+                }
+                conn.Close();
+            }
+            return lstStudent;
+        }
+
+        //AddStudent
+        public void AddStudent(Student student)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spAddStudent", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@FirstName", student.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", student.LastName);
+                cmd.Parameters.AddWithValue("@Email", student.Email);
+                cmd.Parameters.AddWithValue("@Mobile", student.Mobile);
+                cmd.Parameters.AddWithValue("@Address", student.Address);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+    }
+}
---
+++ StudentRecordManagementSystem/StudentRecordManagementSystem/Utility/ConnectionString.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentRecordManagementSystem.Utility
+{
+    public class ConnectionString
+    {
+        private static string cName = "Data Source=KAMLESH\\SQLEXPRESS; Initial Catalog=StudentManagement;User ID=sa;Password=12345678";
+        public static string CName
+        {
+            get => cName;
+        }
+    }
+}
